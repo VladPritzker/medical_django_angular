1dccfe1f57456f6bcc9999aad926b57c283253cb
vlad <pritzkervlad@gmail.com>
2024-05-25 20:45:03 -0400

    styling

--- medical_app/views.py
+++ medical_app/views.py
@@ -5,3 +5,4 @@ def user_list(request):
     users = CustomUser.objects.all().values('username', 'email', 'is_active', 'is_staff', 'is_superuser')
     user_data = list(users)  # Convert the QuerySet to a list of dictionaries
     return JsonResponse(user_data, safe=False)  # Return the data as JSON
+
