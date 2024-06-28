e00211e98d6a49b360e10d9b2cc28de9af7dab16
vlad <pritzkervlad@gmail.com>
2024-06-28 14:33:45 -0400

    test

--- medical_app/views/user_views.py
+++ medical_app/views/user_views.py
@@ -26,7 +26,7 @@ def users(request):
             user = User.objects.create_user(username=username, email=email, password=password)
             return JsonResponse({'message': 'Registration successful', 'user_id': user.user_id, 'username': user.username, 'email': user.email}, status=201)
         
-        
+         
         if action == 'login':
             if not email or not password:
                 return HttpResponseBadRequest("Email and password are required fields")
